e27c4b75042710711b7c34c9bb214c5104c7363a
Brian Olsen <brian@maven-group.org>
2011-07-28 13:24:23 +0200

    Changed config/application to only require RACK_ENV if it is defined

--- lacuna/config/application.rb
+++ lacuna/config/application.rb
@@ -2,5 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, ENV['RACK_ENV']) if defined?(Bundler)
+envs = ENV['RACK_ENV'].nil? ? [:default] : [:default, ENV['RACK_ENV']]
+Bundler.require(*envs) if defined?(Bundler)
 
